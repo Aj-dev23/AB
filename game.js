f8837b231de98d758bf4076539c2123bf22bd96d
Aj-dev23 <poetie125@gmail.com>
2024-10-25 01:19:41 +0200

    Update game.js

--- game.js
+++ game.js
@@ -14,10 +14,10 @@ let gameInterval;
 
 // Afbeeldingen laden
 const ladybugImg = new Image();
-ladybugImg.src = ''; // Vervang dit door de juiste URL
+ladybugImg.src = 'Stripboeken.jpg'; // Relatief pad naar de Ladybug afbeelding
 
 const catNoirImg = new Image();
-catNoirImg.src = ''; // Vervang dit door de juiste URL
+catNoirImg.src = 'cat noir.png'; // Relatief pad naar de Cat Noir afbeelding
 
 // Wacht tot beide afbeeldingen zijn geladen voordat je het spel start
 let imagesLoaded = 0;
@@ -52,3 +52,31 @@ document.addEventListener("keydown", (e) => {
         ladybug.y += 20;
     }
 });
+
+// Spel loop
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Teken Ladybug
+    ctx.drawImage(ladybugImg, ladybug.x, ladybug.y, ladybug.width, ladybug.height);
+
+    // Teken Cat Noir
+    ctx.drawImage(catNoirImg, catNoir.x, catNoir.y, catNoir.width, catNoir.height);
+
+    // Beweeg Cat Noir
+    catNoir.x -= 5;
+    if (catNoir.x < -catNoir.width) {
+        catNoir.x = canvas.width;
+        catNoir.y = Math.random() * (canvas.height - catNoir.height);
+        score += 10; // Verhoog de score als je Cat Noir ontwijkt
+    }
+
+    // Check botsingen
+    if (ladybug.x < catNoir.x + catNoir.width &&
+        ladybug.x + ladybug.width > catNoir.x &&
+        ladybug.y < catNoir.y + catNoir.height &&
+        ladybug.y + ladybug.height > catNoir.y) {
+        alert("Game Over! Your score: " + score);
+        clearInterval(gameInterval);
+    }
+}
